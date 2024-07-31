351ffd0d7fa94f4942abeedbda1735cc0773cc03
bitwiserob <kaczurr@gmail.com>
2024-07-30 21:06:29 -0400

    8

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
-import yfinance as yf
-from datetime import datetime, timedelta
-from nicegui import ui
-import pandas as pd
-from sklearn.linear_model import LinearRegression
-import numpy as np
+from flask import Flask
+app = Flask(__name__)
 
 
-with ui.button('Click me!', on_click=lambda: badge.set_text(int(badge.text) + 1)):
-    badge = ui.badge('0', color='red').props('floating')
-
-
-ui.run()
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
